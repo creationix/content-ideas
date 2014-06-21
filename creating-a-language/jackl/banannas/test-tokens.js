0737f4e1414d2d71ec72dff40d647a12ed0cd9c2
Tim Caswell <tim@creationix.com>
2014-06-21 09:11:46 -0500

    save

--- creating-a-language/jackl/banannas/test-tokens.js
+++ creating-a-language/jackl/banannas/test-tokens.js
@@ -10,20 +10,20 @@ carallel([
   if (err) throw err;
   for (var i = 0; i < tests.length; i += 2) {
     var source = tests[i];
-    var expected = JSON.stringify(JSON.parse(tests[i + 1]));
+    var expected = JSON.parse(tests[i + 1]);
     var tokens = tokenize(source);
-    var actual = JSON.stringify(tokens);
     console.log(source);
-    console.group();
     tokens.forEach(log);
-    if (actual !== expected) {
-      console.error(actual);
-      console.info(expected);
-    }
-    console.groupEnd();
   }
-});
 
-function log(token) {
-  console.log(token);
-}
+  function log(token, i) {
+    if (token === ")") console.groupEnd();
+    console.log(token);
+    if (token === "(") console.group();
+    if (JSON.stringify(token) !== JSON.stringify(expected[i])) {
+      console.error(expected[i]);
+      throw new Error("Token Mismatch");
+    }
+  }
+
+});
