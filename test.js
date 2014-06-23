d6e2f1d392218b5f85c1c81230d4ac1f016f01dd
Tim Caswell <tim@creationix.com>
2014-06-23 12:28:55 -0500

    Move third-party code into own repos.

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
 var readFile = require('./read').file;
-var fs = require('./modules/fs')(__dirname);
-var run = require('./modules/gen-run');
+var fs = require('./modules/tedit-fs/fs')(__dirname);
+var run = require('./modules/gen-run/run');
 var write = require('./write');
-
 
 run(function* () {
   console.log("Loading sample.jkl");
